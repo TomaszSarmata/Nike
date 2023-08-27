ac4d3b714a405045d89cdcf50438c7486c6c5036
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-27 08:18:24 +0100

    created Nav component and imported it into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,11 @@ import {
   Subscribe,
   SuperQuality,
 } from "./sections";
+import Nav from "./components/nav";
 
 const App = () => (
   <main className="relative">
-    {/* <Nav /> */}
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero />
     </section>
