e59c5d4ad29f8f78316b13ef14055277f282016c
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-31 15:51:07 +0100

    made the button render the image programatically

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,13 @@ function Button({ label, iconURL }) {
   return (
     <button className="bg-coral-red text-white px-7 py-4 rounded-full flex justify-center items-center  gap-2 border border-coral-red font-montserrat text-lg leading-none">
       {label}
-      <img
-        src={iconURL}
-        alt="arrow right icon"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 }
