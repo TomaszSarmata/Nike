42b6b70e72622863054b4f255bd22a2339f4e38d
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-05 09:49:22 +0100

    implemented Services section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,15 @@
+import { services } from "../constants";
+import ServiceCard from "../components/ServiceCard";
+
 const Services = () => {
-  return <div>Srvices</div>;
+  console.log(services);
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service}></ServiceCard>
+      ))}
+    </section>
+  );
 };
 
 export default Services;
